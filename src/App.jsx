24370f658129a250ed251a1dfc196206c1dd1304
Sin Cheung Ko <scko823@gmail.com>
2019-03-26 08:30:03 -0700

    get nav set up

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<nav className="Nav">
+						<ul>
+							<li>useState</li>
+							<li>useEffect</li>
+							<li>useContext</li>
+							<li>useReducer</li>
+							<li>useCallback</li>
+							<li>useMemo</li>
+							<li>useRef</li>
+							<li>useImperativeHandle</li>
+							<li>useLayoutEffect</li>
+							<li>useDebugValue</li>
+						</ul>
+					</nav>
+				</header>
+				<h1>something</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
