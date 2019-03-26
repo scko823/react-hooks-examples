2dce5e32380464a728bcc94a0b76574422e7543b
Sin Cheung Ko <scko823@gmail.com>
2019-03-26 08:36:31 -0700

    get router set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<nav className="Nav">
-						<ul>
-							<li>useState</li>
-							<li>useEffect</li>
-							<li>useContext</li>
-							<li>useReducer</li>
-							<li>useCallback</li>
-							<li>useMemo</li>
-							<li>useRef</li>
-							<li>useImperativeHandle</li>
-							<li>useLayoutEffect</li>
-							<li>useDebugValue</li>
-						</ul>
-					</nav>
-				</header>
-				<h1>something</h1>
-			</div>
+			<Router>
+				<div className="App">
+					<header className="App-header">
+						<nav className="Nav">
+							<ul>
+								<li>
+									<Link to="/use-state">useState</Link>
+								</li>
+								<li>
+									<Link to="">useEffect</Link>
+								</li>
+								<li>
+									<Link to="">useContext</Link>
+								</li>
+								<li>
+									<Link to="">useReducer</Link>
+								</li>
+								<li>
+									<Link to="">useCallback</Link>
+								</li>
+								<li>
+									<Link to="">useMemo</Link>
+								</li>
+								<li>
+									<Link to="">useRef</Link>
+								</li>
+								<li>
+									<Link to="">useImperativeHandle</Link>
+								</li>
+								<li>
+									<Link to="">useLayoutEffect</Link>
+								</li>
+								<li>
+									<Link to="">useDebugValue</Link>
+								</li>
+							</ul>
+						</nav>
+					</header>
+					<Route path="/use-state" component={() => <h1>useState</h1>} />
+				</div>
+			</Router>
 		);
 	}
 }
