b1f85e59ccd6344d3fdc78f4f3b0ceee209544e1
Sin Cheung Ko <scko823@gmail.com>
2019-03-26 10:55:23 -0700

    all together 1

---
+++ src/hooks/allTogether.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+const API_URL = 'HTTP://API.SHOUTCLOUD.IO/V1/SHOUT';
+
+const SHOUTEXAMPLE = props => {
+	return (
+		<>
+			<h1>SHOUT!</h1>
+			<div>
+				<div>
+					<input type="text" />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default SHOUTEXAMPLE;
